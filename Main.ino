cc284f686311d6443401f74b5f0fb0d2874d1044
Rahul <87407780+wreck2053@users.noreply.github.com>
2024-09-03 22:10:14 +0530

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,46 +1,60 @@
-#include <SoftwareSerial.h>
+int flexData;
+int flexSensor = A0;
 
-const int flexPin = A0;
-const int trigPin = 9;
-const int echoPin = 10;
+int echoPin = 6;
+int triggerPin = 7;
 
-SoftwareSerial bluetooth(2, 3);
-
+int flexCount = 0;
+int ultrasonicCount = 0;
 void setup() {
     Serial.begin(9600);
-    bluetooth.begin(9600);
-    pinMode(trigPin, OUTPUT);
+    pinMode(flexSensor, INPUT);
     pinMode(echoPin, INPUT);
+    pinMode(triggerPin, OUTPUT);
 }
 
 void loop() {
-    int flexValue = analogRead(flexPin);  // flex sensor
+    // Flex sensor
+    flexData = analogRead(flexSensor);
+    Serial.print("Flex value: ");
+    Serial.print(flexData);
+    Serial.println("");
 
-    long duration, distance;  // ultrasonic sensor
-    digitalWrite(trigPin, LOW);
+    // Ultrasonic sensor
+    digitalWrite(triggerPin, LOW);
     delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
+    digitalWrite(triggerPin, HIGH);
     delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distance = (duration / 2) / 29.1;
+    digitalWrite(triggerPin, LOW);
 
-    Serial.print("Flex level: "); Serial.println(flexValue);
-    Serial.print("Distance: "); Serial.println(distance);
+    long duration = pulseIn(echoPin, HIGH);
+    int dist1 = duration * 0.034 / 2;
+    int dist2 = duration * 0.034 / 2;
+    Serial.print("Distance: ");
+    Serial.print(dist1);
+    Serial.println(" cm");
 
-    bluetooth.print("Flex level: "); bluetooth.println(flexValue);
-    bluetooth.print("Distance: "); bluetooth.println(distance);
-
-    // warning conditions
-    if (distance > 60) {
-        Serial.println("Warning: Object too close!");
-        bluetooth.println("Warning: Object too close!");
+    // GOOD POSTURE
+    // Flex sensor : A constant flex measured at relaxed position of sensor
+    // with an error of + - 20% is allowed.
+    int idealFlex = 145;  // varies due to erroneous sensor
+    int allowedFlexError = (20 * idealFlex) / 100;
+    if (abs(idealFlex - flexData) > allowedFlexError) flexCount++;
+    if (flexCount == 20) {
+        flexCount = 0;
+        // bluetooth, notify the user
     }
 
-    if (flexValue > 250 || flexValue < 150) {
-        Serial.println("Warning: Flex value out of range!");
-        bluetooth.println("Warning: Flex value out of range!");
+    // Ultrasonic sensor : We use 2 ultrasonic sensors (placed diagonally) and
+    // measure the distance on both. This checks if the user's back is parallel
+    // to the chair
+    int idealDifference = 0;
+    int allowedDifference = 10;
+    if (abs(dist1 - dist2) > allowedDifference) ultrasonicCount++;
+    if (ultrasonicCount == 20) {
+        ultrasonicCount = 0;
+        // bluetooth, notify the user
     }
-
+    Serial.println("");
     delay(1000);
 }
